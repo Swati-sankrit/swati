24ad68fe0cebf4ace66d5996550e2ae6ed613623
swati kumari <84333978+Swati-sankrit@users.noreply.github.com>
2021-07-20 20:50:50 +0530

    uploaded files

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+ 
+ var up = document.getElementById("up");
+ var alas = new Audio('./kyun.mp3');
+ /*up.addEventListener('click', ()=>{
+     alas.play();
+})*/
+
+function playAudio(){
+    alas.play();
+}
+ 
